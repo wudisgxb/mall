58569a8f8f6b46787e3fc6d86d456a47446fa3d8
jiang842493698 <842493698@qq.com>
2017-07-03 10:10:21 +0800

    aa

--- app/controller/admin/menus.js
+++ app/controller/admin/menus.js
@@ -69,6 +69,7 @@ module.exports = {
         ctx.checkQuery('tenantId').notEmpty();
         if(ctx.errors){
             ctx.body = new ApiResult(ApiResult.Result.PARAMS_ERROR,ctx.errors );
+            return;
         }
         let menus = await Menus.findAll({
             where: {
