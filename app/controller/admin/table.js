9a8494b40d270f2491072086000ef1fff128db07
jiang842493698 <842493698@qq.com>
2017-07-04 14:05:39 +0800

    22

--- app/controller/admin/table.js
+++ app/controller/admin/table.js
@@ -3,12 +3,90 @@ const ApiResult = require('../../db/mongo/ApiResult')
 const logger = require('koa-log4').getLogger('AddressController')
 let db = require('../../db/mysql/index');
 let Tables = db.models.Tables;
+const ShoppingCarts = db.models.ShoppingCarts;
+const Orders = db.models.Orders;
 
 module.exports = {
+    //获取租户下桌状态
+    async getAdminTableByTableName (ctx, next) {
+        ctx.checkQuery('tenantId').notEmpty();
+        ctx.checkQuery('tableName').notEmpty();
+        if(ctx.errors){
+            ctx.body = new ApiResult(ApiResult.Result.PARAMS_ERROR,ctx.errors );
+        }
+        let table = await Tables.findAll({
+            where: {
+                tenantId: ctx.query.tenantId,
+                name:ctx.query.tableName,
+                consigneeId:null
+            },
+            attributes: {
+                exclude: ['createdAt', 'updatedAt']
+            }
+        })
 
-    async saveAdminTable (ctx, next) {
-        ctx.checkBody('name').notEmpty();
-        ctx.checkBody('info').notEmpty();
+        ctx.body = new ApiResult(ApiResult.Result.SUCCESS, table.status);
+    },
+    //获取租户下 代售点下桌状态
+    async getAdminTableByConsigneeId (ctx, next) {
+        ctx.checkQuery('tenantId').notEmpty();
+        // ctx.checkQuery('tableName').notEmpty();
+        ctx.checkQuery('consigneeId').notEmpty();
+        //ctx.checkQuery('phoneNumber').notEmpty();
+
+        if (ctx.errors) {
+            ctx.body = new ApiResult(ApiResult.Result.PARAMS_ERROR, ctx.errors)
+            return;
+        }
+            //判断是否购物车状态
+        let shoppingCart = await ShoppingCarts.findAll({
+            where: {
+                // phone: ctx.query.phoneNumber,
+                tenantId: ctx.query.tenantId,
+                consigneeId:ctx.query.consigneeId
+            }
+        });
+            //判断是否是订单状态
+        let orders = await Orders.findAll({
+            where: {
+                // phone: ctx.query.phoneNumber,
+                tenantId: ctx.query.tenantId,
+                consigneeId:ctx.query.consigneeId
+            }
+        });
+
+        if (shoppingCart.length > 0) {
+            ctx.body = new ApiResult(ApiResult.Result.SUCCESS, {
+                tableStatus:1,
+                tenantId:shoppingCart.tenantId,
+                phone:shoppingCart.phone
+            });
+            return;
+        } else if(orders.length>0){
+            //判断是否订单状态
+            ctx.body = new ApiResult(ApiResult.Result.SUCCESS, {
+                tableStatus:2,
+                tenantId:orders.tenantId,
+                phone:orders.phone
+
+            });
+            return;
+            //下单状态
+        }else {
+            //空桌
+            ctx.body = new ApiResult(ApiResult.Result.SUCCESS, {
+                tableStatus:0
+            });
+            return;
+        }
+    },
+    //新增租户下桌状态
+    async saveAdminTableByTableName (ctx, next) {
+        ctx.checkBody('/table/name',true).first().notEmpty();
+        ctx.checkBody('/table/status',true).first().notEmpty();
+        ctx.checkBody('/table/status',true).first().notEmpty();
+        ctx.checkBody('/table/status',true).first().notEmpty();
+        ctx.checkBody('/table/status',true).first().notEmpty();
 
         let body = ctx.request.body;
 
@@ -42,6 +120,12 @@ module.exports = {
         }
         ctx.body = new ApiResult(ApiResult.Result.SUCCESS)
     },
+    //新增租户下 代售点下桌号(即代售 桌号)
+    async saveAdminTableByConsigneeId(ctx,next){
+
+    },
+
+    
     async updateAdminTableById (ctx, next) {
         ctx.checkBody('name').notEmpty();
         //ctx.checkBody('status').notEmpty().isInt().ge(0).toInt();
@@ -72,40 +156,7 @@ module.exports = {
         ctx.body = new ApiResult(ApiResult.Result.SUCCESS)
     },
 
-    async getAdminTableByTableName (ctx, next) {
-        ctx.checkQuery('tenantId').notEmpty();
-        ctx.checkQuery('tableName').notEmpty();
-        if(ctx.errors){
-            ctx.body = new ApiResult(ApiResult.Result.PARAMS_ERROR,ctx.errors );
-        }
-        let table = await Tables.findAll({
-            where: {
-                tenantId: ctx.query.tenantId,
-                name:ctx.query.tableName
-            },
-            attributes: {
-                exclude: ['createdAt', 'updatedAt']
-            }
-        })
-        ctx.body = new ApiResult(ApiResult.Result.SUCCESS, table);
-    },
-    async getAdminTableByConsigneeId (ctx, next) {
-        ctx.checkQuery('tenantId').notEmpty();
-        ctx.checkQuery('ConsigneeId').notEmpty();
-        if(ctx.errors){
-            ctx.body = new ApiResult(ApiResult.Result.PARAMS_ERROR,ctx.errors );
-        }
-        let table = await Tables.findAll({
-            where: {
-                tenantId: ctx.query.tenantId,
-                name:ctx.query.tableName
-            },
-            attributes: {
-                exclude: ['createdAt', 'updatedAt']
-            }
-        })
-        ctx.body = new ApiResult(ApiResult.Result.SUCCESS, table);
-    },
+
 
     async deleteAdminTable(ctx, next){
         ctx.checkParams('id').notEmpty().isInt().toInt();
--- app/routers/admin/table.js
+++ app/routers/admin/table.js
@@ -3,9 +3,16 @@
  */
 let table = require('../../controller/admin/table');
 const router = new (require('koa-router'))()
+//获取租户下桌状态
+router.get('/api/v3/admin/deal/table',table.getAdminTableByTableName);
+//获取租户下 代售点下桌状态
+router.get('/api/v3/admin/eshop/table',table.getAdminTableByConsigneeId);
+//新增租户下桌状态
+router.post('/api/v3/admin/deal/table',  table.saveAdminTableByTableName);
+//新增租户下 代售点下桌号(即代售 桌号)
+router.post('/api/v3/admin/eshop/table',  table.saveAdminTableByConsigneeId);
 
-router.post('/api/v3/admin/table/save',  table.saveAdminTable);
+
 router.put('/api/v3/admin/table/save/:id',  table.updateAdminTableById);
-router.get('/api/v3/admin/table',table.getAdminTableByTableName);
 router.delete('/api/v3/admin/table/delete/:id',table.deleteAdminTable);
 module.exports = router
