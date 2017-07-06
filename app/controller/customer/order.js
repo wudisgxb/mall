afe08ccbc5e46a498bf01de24bf37b59ac5a4715
admin <806802194@qq.com>
2017-07-06 21:57:27 +0800

    11

--- app/controller/customer/order.js
+++ app/controller/customer/order.js
@@ -85,7 +85,7 @@ module.exports = {
         }
         result.tableName = table.name;
         result.foods = foodJson;
-        result.traneNo = orders[0].trade_no;
+        result.tradeNo = orders[0].trade_no;
         result.totalNum = totalNum;
         result.totalPrice = Math.round(totalPrice * 100) / 100;
         if (orders[0] != null) {
@@ -540,7 +540,7 @@ module.exports = {
         }
         result.tableName = table.name;
         result.foods = foodJson;
-        result.traneNo = orders[0].trade_no;
+        result.tradeNo = orders[0].trade_no;
         result.totalNum = totalNum;
         result.totalPrice = Math.round(totalPrice * 100) / 100;
         if (orders[0] != null) {
