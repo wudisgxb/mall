6370b8d674e957145a83c8fe7756377d0aded177
admin <806802194@qq.com>
2017-07-11 10:08:03 +0800

    11

--- app/routers/admin/auth.js
+++ app/routers/admin/auth.js
@@ -1,10 +1,9 @@
-
 var db = require('../../db/mysql/index');
 var Admins = db.models.Adminer;
 const auth = require('../../controller/admin/auth');
 const router = new (require('koa-router'))()
-    // todo: redirect
-    router.get('/api/v3/admin/Login',auth.getAdminLoginUser);
-    router.post('/api/v3/admin/Login',auth.getadminLong);
+// todo: redirect
+router.get('/api/v3/admin/Login', auth.getAdminLoginUser);
+router.post('/api/v3/admin/Login', auth.getadminLong);
 module.exports = router
 
