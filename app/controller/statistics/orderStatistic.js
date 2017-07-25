3677213de43818b66e8a51dd3180c3424e22a76b
admin <806802194@qq.com>
2017-07-25 15:31:03 +0800

    11

--- app/controller/statistics/orderStatistic.js
+++ app/controller/statistics/orderStatistic.js
@@ -26,6 +26,8 @@ const getstatistics = (function () {
             platformCouponFee:json.platformCouponFee,
             merchantCouponFee:json.merchantCouponFee,
             phone:json.phone,
+            tenantId:json.tenantId,
+            consigneeId:json.consigneeId,
         })
     }
     // 查询平均消费
