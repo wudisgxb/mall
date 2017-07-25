f11ac86538504eacb218336c94030502603ec287
admin <806802194@qq.com>
2017-07-25 12:10:29 +0800

    11

--- app/routers/admin/consignees.js
+++ app/routers/admin/consignees.js
@@ -5,7 +5,7 @@ const consignees = require('../../controller/admin/consignees');
 const router = new (require('koa-router'))()
 // todo: redirect
 router.get('/api/test/admin/consignee',consignees.getAdminConsignees);
-//router.post('/api/v3/admin/consignee',consignees.saveAdminConsignees);
+router.post('/api/v3/admin/consignee',consignees.saveAdminConsignees);
 router.put('/api/test/admin/consignee',consignees.updateAdminConsignees);
 //router.delete('/api/v3/admin/consignee',consignees.deleteAdminConsignees);
 module.exports = router
