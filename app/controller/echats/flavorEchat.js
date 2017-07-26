6976d8962ca3820e77a532e8909f8a8a7e999c08
jiang842493698 <842493698@qq.com>
2017-07-26 09:41:50 +0800

    asasas

--- app/controller/echats/flavorEchat.js
+++ app/controller/echats/flavorEchat.js
@@ -1,15 +1,37 @@
-let db = require('../../db/mysql/index');
-let Orders = db.models.Orders;
-let Foods = db.models.Foods;
+const db = require('../../db/statisticsMySql/index');
+const dbc = require('../../db/MySql/index')
+const  Foods = dbc.models.Foods;
+const Orders = dbc.models.Orders;
+const Foods = db.models.Foods;
 
 const getFindCount = (function () {
 
     let getCount = async function (tenantId,startTime,endTime,type,foodname) {
         let getTime;
         if(type==1){
-             getTime = await getDayEchat.getDay(startTime,endTime)
+            getTime = await getDayEchat.getDay(startTime,endTime)
         }
-        let orderMenus = await Orders.find()
+        for (let i = 0; i < getTime.length; i++){
+            let food =[]
+            let orderMenus = await Orders.findAll({
+                where:{
+                    tenantId:tenantId,
+                    createdAt:{
+                        $gt:getTime[i].start,
+                        $lt:getTime[i].end
+                    }
+                }
+            })
+            for (let j = 0;j<orderMenus.length;j++){
+                if(food.contains()){
+
+                }
+            }
+
+
+
+        }
+
     }
     let instance = {
         getCount: getCount
