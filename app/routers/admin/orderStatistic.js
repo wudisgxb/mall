a3e8b4379d52222948d8ccf38b7bc801cf1c7212
jiang842493698 <842493698@qq.com>
2017-07-31 20:12:41 +0800

    asasasa

--- app/routers/admin/orderStatistic.js
+++ app/routers/admin/orderStatistic.js
@@ -8,7 +8,7 @@ router.post('/api/test/admin/orderStatistic', orderStatistic.getOrderStatistic);
  router.post('/api/test/admin/ordersStatistic', orderStatistic.saveOrderStatistic);
 router.get('/api/test/admin/ordersStatistic',orderStatistic.getAllOrderStatistic);
 
-router.put('/api/test/admin/ordersStatistic',orderStatistic.putOrderStatistic)
+// router.put('/api/test/admin/ordersStatistic',orderStatistic.putOrderStatistic)
 router.put('/api/test/admin/orders/ordersStatistic',orderStatistic.updateOrder)
 
 router.put('/api/test/admin/ordersStatistic',orderStatistic.putOrderStatistic);
