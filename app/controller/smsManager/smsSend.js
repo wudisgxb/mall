2dd786e7438d6b3ac6e8c73bde3ab20f0c387414
admin <806802194@qq.com>
2017-08-01 18:05:46 +0800

    短信平台切换

--- app/controller/smsManager/smsSend.js
+++ app/controller/smsManager/smsSend.js
@@ -6,33 +6,40 @@ var zy = {
     // set setConfig(config) {
     //     this.config = config;
     // },
-    config : {
+    config: {
+        apiAccount: 'ACCd865926c10ea49719632b76b67560f06',
 
-        appKey: 'e78e86d9912946ad99828a5b386d8435',
+        apiKey: 'API522c6e001f514e1ea31e3a4660ac7730',
 
-        token: '41Om3Xvk158L',
+        //token: '41Om3Xvk158L',
 
+        templateId: 'mtldUDX6ZymEebOQ',
 
-        templateId: 'CJF477RCMM8K'
+        singerId: 'msn9E415cjHH1bQj',
     },
-    sendSms: function(mobile, code, callback) {
-        var timestamp = Math.round(new Date().getTime()/1000);
-        var sign = crypto.createHash('md5').update(this.config.appKey + this.config.token
-            + this.config.templateId + mobile + code + timestamp, 'utf8').digest("hex");
-        var url = 'https://sms.zhiyan.net/sms/sms/single/' + this.config.appKey + '/' +
-            this.config.token + '/' + this.config.templateId + '?timestamp=' + timestamp + '&sign=' + sign;
-        var data = JSON.stringify({
-            mobile: mobile,
-            param: code,
-            extend: ''
-        });
+    sendSms: function (mobile, code, callback) {
+        //var timestamp = Math.round(new Date().getTime()/1000);
+        var timestamp = new Date().getTime();
+        var sign = crypto.createHash('md5').update(this.config.apiAccount + this.config.apiKey + timestamp, 'utf8').digest("hex");
+        var url = 'http://www.zypaas.com:9988/V1/Account/ACCd865926c10ea49719632b76b67560f06/sms/sureTempalteSend';
+        var data = {
+            "apiAccount": "ACCd865926c10ea49719632b76b67560f06",
+            "appId": "APPa013394712af44ef9d777654f7774cfc",
+            "sign": sign,
+            "timeStamp": timestamp,
+            "templateId": this.config.templateId,
+            "singerId": this.config.singerId,
+            "mobile": mobile,
+            "param": code,
+        };
         var opt = {
-            rejectUnauthorized: false,
             url: url,
             method: 'POST',
-            form: {
-                data: data
-            }
+            json: true,
+            headers: {
+                "content-type": "application/json",
+            },
+            body: data
         };
         request(opt, callback);
     }
