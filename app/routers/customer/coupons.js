35e1ce7a55521867191ce0b1075dd90ad087ed91
admin <806802194@qq.com>
2017-08-19 16:37:17 +0800

    删除优惠券

--- app/routers/customer/coupons.js
+++ app/routers/customer/coupons.js
@@ -1,5 +1,5 @@
 let coupon = require('../../controller/customer/coupon');
-
+let couponAdmin = require('../../controller/admin/coupon');
 const router = new (require('koa-router'))()
 
 router.post('/api/test/customer/deal/coupon', coupon.bindCoupon);
@@ -14,5 +14,8 @@ router.get('/api/test/customer/deal/availableCoupon', coupon.getAvailableCoupon)
 //独立接口 优惠券绑定订单号
 router.post('/api/test/customer/eshop/couponBindTradeNo', coupon.couponBindTradeNo);
 router.post('/api/test/customer/deal/couponBindTradeNo', coupon.couponBindTradeNo);
+
+router.delete('/api/test/customer/eshop/coupon', couponAdmin.deleteAdminCoupon);
+router.delete('/api/test/customer/deal/coupon', couponAdmin.deleteAdminCoupon);
 
 module.exports = router
