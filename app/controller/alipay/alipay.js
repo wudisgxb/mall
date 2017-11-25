ef3014ce51a8aaba4e02f67224a74f80e08ab0cb
jiang842493698 <842493698@qq.com>
2017-11-25 15:13:27 +0800

    修改桌号

--- app/controller/alipay/alipay.js
+++ app/controller/alipay/alipay.js
@@ -649,7 +649,7 @@ module.exports = {
                                         "color": "#173177"
                                     },
                                     "keyword2": {
-                                        "value": table.name,
+                                        "value": table.name=="0号桌"?order.info:table.name,
                                         "color": "#173177"
                                     },
                                     "keyword3": {
